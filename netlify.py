c71709fc07a6d4b83867fa4eaaaca4839d5c6bcf
blackice <45483405+lwhx@users.noreply.github.com>
2023-06-25 21:42:39 +0800

    Update and rename netlify.py to netlify.toml

--- netlify.py
+++
@@ -1,2 +0,0 @@
-[build]
-  command = "docker run -d --name chat-gpt -p 8899:8899 -e PUID=0 -e PGID=0 -e TZ=Asia/Shanghai -e PANDORA_SERVER=0.0.0.0:8899 -e PANDORA_CLOUD=cloud -e PANDORA_ACCESS_TOKEN= --restart always pengzhile/pandora"
