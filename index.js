207da08134bd97a1137c9c7310978481e6d45705
blackice <45483405+lwhx@users.noreply.github.com>
2023-06-25 21:42:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-const port = process.env.PORT || 8899;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
